18d4b01a72a4a878b2fb113d353b5685f38fae2d
SankalpSTG <sankalppolk123@gmail.com>
2022-05-26 00:45:21 +0530

    Create 6.py

---
+++ arrays/6.py
@@ -0,0 +1,11 @@
+def driver(N, arr, K):
+    arr.sort(reverse=True)
+    return arr[K - 1]
+
+def main():
+    N = int(raw_input())
+    arr = list(map(int, raw_input().split()))
+    K = int(raw_input())
+    print(driver(N, arr, K))
+
+main()
